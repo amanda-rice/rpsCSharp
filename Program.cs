0b3ba1b27fe03a3eb253dd81fa44ab5da6bedb35
amanda-rice <aricericerice@gmail.com>
2021-08-30 14:57:52 -0600

    fixed tabs

--- Program.cs
+++ Program.cs
@@ -4,32 +4,32 @@ using rpsCSharp.Models;
 
 namespace rpsCSharp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
       bool isPlaying = true;
-            while(isPlaying)
-            {
+      while(isPlaying)
+      {
         getInput();
       }
-        }
-        public static void getInput()
-        {
+    }
+    public static void getInput()
+    {
       Console.WriteLine("Rock, Paper Scissors!");
-            string userChoice = Console.ReadLine();
-            if(userChoice.ToUpper() != "ROCK" && userChoice.ToUpper() != "PAPER" && userChoice.ToUpper() != "SCISSORS")
-            {
-                Console.WriteLine("Please enter rock, paper, or scissors");
-                getInput();
-            }
-            else
-            {
-                Game myGame = new Game();  
-                myGame.GetCompChoice();
-                myGame.UpdateUserChoice(userChoice);
-                Console.WriteLine(myGame.Play());
-            }
+      string userChoice = Console.ReadLine();
+      if(userChoice.ToUpper() != "ROCK" && userChoice.ToUpper() != "PAPER" && userChoice.ToUpper() != "SCISSORS")
+      {
+          Console.WriteLine("Please enter rock, paper, or scissors");
+          getInput();
+      }
+      else
+      {
+          Game myGame = new Game();  
+          myGame.GetCompChoice();
+          myGame.UpdateUserChoice(userChoice);
+          Console.WriteLine(myGame.Play());
+      }
     }
-    }
+  }
 }
